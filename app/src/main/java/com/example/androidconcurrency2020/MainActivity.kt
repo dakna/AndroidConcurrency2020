5c235a077d547d8e2a8e3a20c9ab6b31eb8fd57a
TWOMEN\dank <Daniel.Knapp@twomen.com>
2020-07-24 11:47:14 -0400

    challenge solution

--- app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.androidconcurrency2020
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 import kotlin.random.Random
+
+const val DIE_VIEW_INDEX_KEY = "dieViewIndexKey"
+
+const val DIE_DRAWABLE_INDEX_KEY = "dieDrawableIndexKey"
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var imageViews: Array<ImageView>
+
+    val drawables = arrayOf(
+        R.drawable.die_1,
+        R.drawable.die_2,
+        R.drawable.die_3,
+        R.drawable.die_4,
+        R.drawable.die_5,
+        R.drawable.die_6
+    )
+
+    val handler = Handler(Looper.getMainLooper()) {
+        val drawable = drawables[it.data.getInt(DIE_DRAWABLE_INDEX_KEY)]
+        val view = imageViews[it.data.getInt(DIE_VIEW_INDEX_KEY)]
+        view.setImageResource(drawable)
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         // Initialize view binding for view object references
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
 
         binding.rollButton.setOnClickListener { rollTheDice() }
 
@@ -25,21 +55,22 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollTheDice() {
 
-        val imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
-        val drawables = arrayOf(
-            R.drawable.die_1,
-            R.drawable.die_2,
-            R.drawable.die_3,
-            R.drawable.die_4,
-            R.drawable.die_5,
-            R.drawable.die_6
-        )
-
-        for (dieIndex in imageViews.indices) {
-            val dieNumber = getDieValue()
-            imageViews[dieIndex].setImageResource(drawables[dieNumber - 1])
+        thread(start = true) {
+            for (i in 1..20) {
+                for (dieIndex in imageViews.indices) {
+                    val dieNumber = getDieValue()
+                    val bundle = Bundle().apply {
+                        putInt(DIE_VIEW_INDEX_KEY, dieIndex)
+                        putInt(DIE_DRAWABLE_INDEX_KEY, dieNumber - 1 )
+                    }
+                    Message().also {
+                        it.data = bundle
+                        handler.sendMessage(it)
+                    }
+                }
+                Thread.sleep(100)
+            }
         }
-
     }
 
     /**
